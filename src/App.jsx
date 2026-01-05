dd21ee66e83c0ace4cbd63d72e05da391d71342b
ishansivmal <ishansivmal@gmail.com>
2026-01-05 14:43:18 +0530

    day 11

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from 'react'
 
 import './App.css'
 import ProductCard from './components/product-card'
+import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (<>
+ <div className="w-[600px] h-[600px] bg-gray-600 relative border-2 relative ">
+      <div className="bg-amber-300 w-[500px] h-[500px] flex flex-col justify-center items-center  ">
+            <div className='w-[100px] h-[100px] bg-blue-500 fixed left-[550px] top-[300px]'>
 
-  <ProductCard name = "laptop 1" price = "1200$" img = "https://picsum.photos/id/1/200/300"/> 
-  <ProductCard name = "laptop 2" price = "1300$" img = "https://picsum.photos/id/2/200/300"/> 
-  <ProductCard name = "laptop 3" price = "1400$" img = "https://picsum.photos/id/3/200/300"/> 
+            </div>
+            <div className='w-[100px] h-[100px] bg-red-500 absolute bottom-[100px] right-[100px]'>
+
+            </div>
+            <div className='w-[100px] h-[100px] bg-pink-500'>
+
+
+            </div>
+            <div className='w-[100px] h-[100px] bg-green-500'>
+
+            </div>
+      </div>
+ </div>
   </>)
  
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import UserData from "./userData";
+
+export default function Header() {
+    {
+      return (
+        <header>
+          <h1>My Website</h1>
+         <UserData/>
+        </header>
+      );
+    }
+}
