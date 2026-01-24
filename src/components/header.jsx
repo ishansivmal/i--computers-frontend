4feb6f18f8d67f00f85837bc36cda1b4dd17ba2a
ishansivmal <ishansivmal@gmail.com>
2026-01-24 14:34:56 +0530

    day -21/22 - finished ❤️

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import { BiShoppingBag } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import { CiMenuBurger } from "react-icons/ci";
+import { LuListCollapse } from "react-icons/lu";
 import { useState } from "react";
 
 
@@ -10,7 +10,11 @@ export default function Header() {
     {
       return (
         <header className=" w-full h-[100px] bg-accent flex relative">
-          <CiMenuBurger className="text-white my-auto p-[1px] text-4xl ml-[6px] lg:hidden" />
+          <LuListCollapse
+          onClick={() => setSidebarOpen(true)}
+
+           className="text-white my-auto p-[1px] text-4xl ml-[6px] lg:hidden" />
+          
 
           <img src="logo.png"className="h-full" alt="My Shop" />
           <div className="w-full h-full hidden lg:flex  text-primary justify-center items-center gap-[30px] text-xl">
@@ -23,17 +27,35 @@ export default function Header() {
         <Link to="/cart" className="absolute right-10 top-1/2 transform -translate-y-1/2 text-primary text-2xl font-bold">
         <BiShoppingBag />
         </Link>
-        <div className="  w-[100vw] h-screen top-0 left-0 fixed  bg-black/50 z-40">
+       {sidebarOpen && <div className=" lg:hidden w-[100vw] h-screen top-0 left-0 fixed  bg-black/50 z-40">
           
-          <div className="fixed flex-col  border-2 relative w-[300px] h-screen left-0">
-            <div className="absolute w-full h-full  bg-white-600 left[-300px]transform-flat translate-x-[-300px] transition-transform duration-1000 flex flex-col"> 
-                <div className="w-full h-[100px] bg-acccent  flex justify-center items-center">
-                  <img src="/logo.png" alt="logo" className="h-full" />
+          <div className="fixed flex-col  border-2  w-[250px] h-screen relative">
+            
+                <div className=" flex flex-col w-full h-full bg-white left-[-250px] translate-x-[0px] transition-transform duration-1000">
+                  <div className="h-[100px] w-full bg-accent flex justify-center items-center">
+                    <img src="logo.png"className="h-full" alt="My Shop" />
+                              <LuListCollapse 
+                              onClick={() => setSidebarOpen(false)}
+                              className="text-white my-auto p-[1px] text-4xl ml-[6px] lg:hidden rotate-180" />
+                    
+
+                  </div>
+                  <div className="flex flex-col gap-[20px] mt-[30px] text-black ">
+                    
+                    {/* home */} 
+                    <a href="/" className="ml-[12px] text-primary text-xl text-center text-black text-left ">Home</a>
+                    {/* products */}
+                    <a href="/products" className="ml-[12px] text-primary text-xl text-center text-black text-left ">Products</a>  
+                    
+                    {/* about */}
+                    <a href="/about" className="ml-[12px] text-primary text-xl text-center text-black text-left ">About</a>
+
+                  </div>
                 </div>
-            </div>  
+              
           </div>
 
-        </div>
+        </div>}
         </header>
         
       );
